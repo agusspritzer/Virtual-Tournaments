45e60aa2fcaaade7fb36408248d0e28140bb3c7b
gonzarascon <gonzarascon@gmail.com>
2020-06-08 21:56:14 -0300

    Import fonts + dynamic import twitch

--- components/Main/index.js
+++ components/Main/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import TwitchEmbedVideo from "react-twitch-embed-video"
+import dynamic from 'next/dynamic';
 
+const TwitchEmbedVideo = dynamic(import('react-twitch-embed-video'), {
+  ssr: false,
+});
 
-
-class Main extends React.Component{
-    render (){
-        return (
-
-            <TwitchEmbedVideo
-            autoplay
-            height="480"
-            muted
-            targetId="twitch-embed"
-            channel="brunenger"
-            layout="video"
-            width="940"
-          />          
-
-        )
-    }
+class Main extends React.Component {
+  render() {
+    return (
+      <TwitchEmbedVideo
+        autoplay
+        height="480"
+        muted
+        targetId="twitch-embed"
+        channel="brunenger"
+        layout="video"
+        width="940"
+      />
+    );
+  }
 }
 
 export default Main;
