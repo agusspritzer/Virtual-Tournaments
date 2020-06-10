4610448c4e7b339d36da8a4814c1e479fa2b9434
agus522 <51430029+agus522@users.noreply.github.com>
2020-06-10 02:31:46 -0300

    Nuevos cambios 9-06

--- components/Main/index.js
+++ components/Main/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import {Stream, H3, H4, H5, ContenedorMain, Juegos, Card} from './styled';
+import {Stream, H1 , H3, H4, H5,Titulo, Beneficios, Presentacion, Foto, ContenedorMain, Juegos, Card} from './styled';
 
 const TwitchEmbedVideo = dynamic(import('react-twitch-embed-video'), {
   ssr: false,
@@ -11,6 +11,51 @@ class Main extends React.Component {
     return (
 
       <ContenedorMain>
+
+        <Presentacion>
+
+        <Titulo>
+        <H1>ACTIVA TU MODO TRYHARD EN</H1>
+        <H1  style={{color: "#EA33BC"}} >VIRTUAL TOURNAMENTS</H1>
+
+        <H4>Virtual Tournaments es la plataforma amateur definitiva
+        que te ofrece la posibilidad de inscribirte a torneos de tus
+        juegos preferidos para demostrar que sos el mejor en ellos!</H4>
+        </Titulo>
+
+        <Foto>
+        <img src="../assets/presentacion.png"/>
+        </Foto>
+
+        </Presentacion>
+
+
+        <H3 style={{textAlign: "center"}}>¡Competir desde casa nunca tuvo tantas ventajas!</H3>
+        
+      <Beneficios>
+    
+      <div>
+        <img src="../assets/beneficios/trofeo.png"/>
+        <p>Podrás acceder a grandes 
+premios monetarios por ganar 
+torneos de tus juegos favoritos</p>
+      </div>
+      <div>
+        <img src="../assets/beneficios/twitch.png"/>
+        <p>Contar con la posibilidad de que tus 
+partidas sean vistas por miles de 
+jugadores a través de Twitch</p>
+      </div>
+      <div>
+        <img src="../assets/beneficios/personas.png"/>
+        <p>Formar parte de una comunidad que 
+apoya la escena local de jugadores 
+no profesionales en E-Sports</p>
+      </div>
+
+      </Beneficios>
+
+
 
         <Stream>
           <H3>¡Estamos transmitiendo en vivo ahora!</H3>
@@ -25,6 +70,8 @@ class Main extends React.Component {
                     width="800"
               />
         </Stream>
+
+
 
         <H3>Demostra ser el mejor en tu juegos favoritos</H3>
 
--- components/Main/styled.js
+++ components/Main/styled.js
@@ -10,7 +10,47 @@ const ContenedorMain = styled.div`
 
 width:65%;
 margin:0 auto;
-padding-top: 7%;
+padding-top: 10%;
+
+`
+
+const H1 = styled.h1`
+*{
+    padding:0;
+    margin:0;
+}
+
+        font-family: 'TTOctosquares';
+        font-size: 2em;
+        font-weight:400;
+`
+
+const Titulo = styled.div`
+
+width:50%;
+`
+const Foto = styled.div`
+
+width:50%;
+
+img{
+    margin:0 auto;
+    width:55%;
+    height:55%;
+}
+`
+
+const Presentacion = styled.div`
+
+width: 100%;
+display: flex;
+flex-wrap: nowrap;
+justify-content: flex-start;
+align-items: center;
+padding-bottom: 15%;
+padding-top: 2%;
+
+
 
 `
 const H3 = styled.h3`
@@ -18,14 +58,15 @@ const H3 = styled.h3`
         font-family: 'TTOctosquares';
         font-size:1.6em;
         font-weight:700;
-        padding:1%;
+        padding-bottom: 2%;
 `
 
 const H4 = styled.h4`
 
 font-size:1em;
-    font-weight:500;
+    font-weight:400;
     color: #B5B5B5;
+    line-height: 30px;
     padding-bottom: 2%;
 `
 
@@ -46,9 +87,12 @@ const Stream = styled.div`
     margin: 0 auto;
     text-align: center;
     background-color: #272727;
+    padding-top:8%;
     padding-bottom:8%;
     
 `
+
+
 
 const Juegos = styled.div`
     width: 100%;
@@ -69,4 +113,33 @@ const Card = styled.div`
     }
 `
 
-export { Stream, Card, H3, H4, H5, ContenedorMain, Juegos };
+const Beneficios = styled.div`
+
+    text-align: center;
+    background-color: #272727;
+    padding-top:5%;
+    padding-bottom:8%;
+    display: flex;
+    justify-content: space-around;
+
+    div{
+        width: 22%;
+        height: 150px;
+    }
+
+    img{
+        width: auto;
+        height: 35%;
+        margin: 0 auto;
+    }
+
+    p{
+        font-size: 0.8em;
+    font-weight:500;
+    color: #B5B5B5;
+    line-height: 25px;
+    padding-top: 8%;
+    }
+`
+
+export { Stream, Presentacion, Titulo, Foto, Beneficios, Card, H1, H3, H4, H5, ContenedorMain, Juegos };
