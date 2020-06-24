f81a618b47ab2d714ebb7281f8fa177174748342
gonzarascon <gonzarascon@gmail.com>
2020-06-23 23:10:15 -0300

    Titulo index.js

--- components/Titulo/index.js
+++ components/Titulo/index.js
@@ -1,43 +1,24 @@
 import React from 'react';
-import {H1, H2, H3, H4, H5} from './styled';
+import { H1, H2, H3, H4, H5 } from './styled';
 
-export default function Titulo ({level, children}) {
- 
-              switch(level) {
-            case 0:
-              return(
-                <H1>{children}</H1>
-                 ) 
+export default function Titulo({ level, children }) {
+  switch (level) {
+    case 0:
+      return <H1>{children}</H1>;
 
-            case 1:
-              return(
-                <H2>{children}</H2>
-              )
+    case 1:
+      return <H2>{children}</H2>;
 
-            
-            case 3:
-              return(
-                <H3>{children}</H3>
-                 )
+    case 3:
+      return <H3>{children}</H3>;
 
+    case 4:
+      return <H4>{children}</H4>;
 
-            case 4:
-              return(
-                <H4>{children}</H4>
-                 ) 
+    case 5:
+      return <H5>{children}</H5>;
 
-
-            case 5:
-              return(
-                <H5>{children}</H5>
-              )
-            
-            default:
-              return(
-              <H3>Titulo default</H3>
-               )
-
-          }
-        }
-
-        
+    default:
+      return <H3>{children}</H3>;
+  }
+}
