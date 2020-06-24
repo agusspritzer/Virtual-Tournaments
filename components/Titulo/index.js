a4c3dbb9446ffe4953997528d8510e3875ebe107
agus522 <51430029+agus522@users.noreply.github.com>
2020-06-23 23:06:04 -0300

    Nuevos cambios 23-06

--- components/Titulo/index.js
+++ components/Titulo/index.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import {H1, H2, H3, H4, H5} from './styled';
 
-export default function Titulo ({level}) {
+export default function Titulo ({level, children}) {
  
               switch(level) {
             case 0:
               return(
-                <H1>Esto es H1</H1>
+                <H1>{children}</H1>
                  ) 
 
             case 1:
               return(
-                <H2>Esto es H2</H2>
+                <H2>{children}</H2>
               )
 
             
             case 3:
               return(
-                <H3>Esto es H3</H3>
+                <H3>{children}</H3>
                  )
 
 
             case 4:
               return(
-                <H4>Esto es H4</H4>
+                <H4>{children}</H4>
                  ) 
 
 
             case 5:
               return(
-                <H5>Esto es H5</H5>
+                <H5>{children}</H5>
               )
             
             default:
               return(
-              <H3>Titulo Default</H3>
+              <H3>Titulo default</H3>
                )
 
           }
