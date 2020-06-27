96626b89a2542133141bb87aa5296903e98c10cb
agus522 <51430029+agus522@users.noreply.github.com>
2020-06-26 23:05:40 -0300

    Cambios

--- containers/ProyectoContainer/index.js
+++ containers/ProyectoContainer/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import "~slick-carousel/slick/slick.css"; 
-import "~slick-carousel/slick/slick-theme.css";
+<<<<<<< HEAD
+=======
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+>>>>>>> ffa4354cc3bc514cb9ae503d53f287feb09fcca5
 
 import {
   Principal,
@@ -9,8 +12,6 @@ import {
   Testimonios,
 } from './styled';
 
-
-
 import { ContenedorMain } from 'components';
 import { FullWidthSection } from 'components';
 import { Titulo } from 'components';
@@ -18,47 +19,40 @@ import { Quote } from 'components';
 import { Slider } from 'components';
 
 export default function ProyectoContainer() {
-  return <>
-<ContenedorMain>
-<Presentacion>
+  return (
+    <>
+      <ContenedorMain>
+        <Presentacion>
           <PresentacionPersona>
             <figure>
-            <img src="/assets/players/prueba.jpg" />
+              <img src="/assets/players/prueba.jpg" />
             </figure>
             <Titulo level={4}>Agustín Spritzer</Titulo>
-            <p>Fundador de Virtual Tournaments</p>            
-
+            <p>Fundador de Virtual Tournaments</p>
           </PresentacionPersona>
 
           <Principal>
-            <Titulo level={1}>VIRTUAL TOURNAMENTS, UN LUGAR HECHO A TU MEDIDA</Titulo>
+            <Titulo level={1}>
+              VIRTUAL TOURNAMENTS, UN LUGAR HECHO A TU MEDIDA
+            </Titulo>
             <p>
-            El principal objetivo fue crear una plataforma que unifique los 
-            torneos amateurs que se juegan todos los dias. Era darle una 
-            posiblidad al gamer no profesional un espacio donde participar en 
-            torneos abiertos donde pueda mejorar su nivel de habilidad
+              El principal objetivo fue crear una plataforma que unifique los
+              torneos amateurs que se juegan todos los dias. Era darle una
+              posiblidad al gamer no profesional un espacio donde participar en
+              torneos abiertos donde pueda mejorar su nivel de habilidad
             </p>
           </Principal>
+        </Presentacion>
+      </ContenedorMain>
 
+      <FullWidthSection>
+        <Testimonios>
+          <Titulo level={5}>TESTIMONIOS</Titulo>
+          <Slider></Slider>
+        </Testimonios>
+      </FullWidthSection>
 
-</Presentacion>
-</ContenedorMain>
-
-<FullWidthSection>
-
-
-  <Testimonios>
-    <Titulo level={5}>TESTIMONIOS</Titulo>
-    <Slider></Slider>
-  </Testimonios>
-
-
-</FullWidthSection>
-
-<ContenedorMain>
-Lo que iene despues del fulll section
-</ContenedorMain>
-
-
-  </>;
+      <ContenedorMain>Lo que iene despues del fulll section</ContenedorMain>
+    </>
+  );
 }
