6e19e8cd62810ffe68f85f6f7de3987ca8a7901d
Facundo Brahim <facundo.e.brahim@gmail.com>
2021-11-23 22:09:54 -0300

    Header Hover Fixed

--- components/Header/styled.js
+++ components/Header/styled.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import media from 'lib/constants/media'
+import media from 'lib/constants/media';
 
 {
   /* Aca traigo el styled para escribir el css*/
@@ -20,18 +20,17 @@ const ContenedorHeader = styled.div`
   padding: 1% 3% 1% 3%;
   display: flex;
   justify-content: space-between;
-align-items: center; 
-position: fixed;
-width:100%;
+  align-items: center;
+  position: fixed;
+  width: 100%;
 
-box-shadow: 4px 8px 20px -6px rgba(0,0,0,0.75);
+  box-shadow: 4px 8px 20px -6px rgba(0, 0, 0, 0.75);
 
   nav {
     width: 45%;
   }
-  
-  
-  nav ul li{
+
+  nav ul li {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -42,82 +41,76 @@ box-shadow: 4px 8px 20px -6px rgba(0,0,0,0.75);
     color: #eeeeee;
     font-size: 0.8em;
     font-weight: 500;
-   letter-spacing: 0.4px;
-   position: relative;
-  transition: 0.2s ease 0s;
+    letter-spacing: 0.4px;
+    position: relative;
+    transition: 0.2s ease 0s;
     font-family: 'Maison Neue Extended';
     font-family: 'TTOctosquares';
   }
 
-a:after{
+  a:after {
     position: absolute;
     bottom: 0px;
     left: 0px;
     width: 100%;
     height: 3px;
-    background-color: #6418DC;
-    content: "";
+    background-color: #6418dc;
+    content: '';
     opacity: 0;
     transition: opacity 0.2s ease 0s, transform 0.2s ease 0s;
     transform: translateY(8.5px);
     cursor: pointer;
-}
+  }
 
-a:hover:after{
+  a:hover:after {
     opacity: 1;
     transform: translateY(4.5px);
-    background-color: #6418DC;
+    background-color: #6418dc;
   }
 
-
-  img{
-    height:100%;
-    width:100%
+  #nonAfter:hover:after {
+    opacity: 0 !important;
   }
 
-  nav ul li div{
+  img {
+    height: 100%;
+    width: 100%;
+  }
+
+  nav ul li div {
     display: flex;
     align-items: center;
     cursor: pointer;
   }
-  nav ul li div a{
+  nav ul li div a {
     font-family: 'Maison Neue Extended';
-   margin-left: 15px;
-   font-size: 1em;
-   font-weight: 400;
-
-
+    margin-left: 15px;
+    font-size: 1em;
+    font-weight: 400;
   }
 
-  nav ul li div img{
-height:  40px;
-width: 40px;
-border-radius: 200px;
-object-fit: cover;
+  nav ul li div img {
+    height: 40px;
+    width: 40px;
+    border-radius: 200px;
+    object-fit: cover;
   }
 
-
-  @media (max-width: ${media.smallDevice}){
-    nav{
-      display:none;
+  @media (max-width: ${media.smallDevice}) {
+    nav {
+      display: none;
     }
-    
-}
-
-
+  }
 `;
 
 const Logo = styled.div`
+  width: 10%;
+  cursor: pointer;
 
-    width: 10%;
-    cursor: pointer;
-
-    @media (max-width: ${media.smallDevice}){
-      padding: 3%;
-      width: 35%;
-    }
-
+  @media (max-width: ${media.smallDevice}) {
+    padding: 3%;
+    width: 35%;
+  }
 `;
-
 
 export { ContenedorHeader, Logo };
