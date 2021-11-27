e3d85bff2ef4b88f2fa7e913961d009c69371f05
Agustin Spritzer <agus.spritzer22@gmail.com>
2021-11-27 11:46:52 -0300

    Responsive Login Register

--- containers/LoginContainer/styled.js
+++ containers/LoginContainer/styled.js
@@ -23,6 +23,45 @@ p{
 
 }
 
+@media (max-width: ${media.desktop1680}) {
+    padding-top: 3%;
+    width: 48%;
+  }
+  @media (max-width: ${media.desktop1366}) {
+    h2{
+  font-size: 1.6em;
+}
+
+a{
+    font-size: 0.9em;
+}
+    padding-top: 6%;
+    width: 45%;
+  }
+
+  @media (max-width: ${media.tablet1024}) {
+    padding-top: 9%;
+    width: 60%;
+    a{
+    font-size: 0.82em;
+}
+  }
+
+  @media (max-width: ${media.mobile651}) {
+    padding-top: 14%;
+    width: 85%;
+    a{
+    font-size: 0.7em;
+}
+
+  }
+
+  @media (max-width: ${media.mobile375}) {
+    padding-top: 22%;
+    padding-bottom: 10%;
+    width: 95%;
+  }
+
 
 `;
 
@@ -34,7 +73,7 @@ justify-content: space-between;
 flex-direction: column;
 
 
-@media ${media.mediumDevice} {
+@media ${media.tablet1024} {
     flex-direction: row;
 }
 `;
--- containers/RegistroContainer/styled.js
+++ containers/RegistroContainer/styled.js
@@ -1,50 +1,80 @@
 import styled from 'styled-components';
-import media from 'lib/constants/media'
+import media from 'lib/constants/media';
 
 const DataContainer = styled.form`
   width: 45%;
   margin: 0 auto;
-display:flex;
-flex-direction: column;
-justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
-h2{
+  h2 {
     text-align: center;
     margin-bottom: 35px;
-}
-p{
+  }
+  p {
     margin-top: 15px;
     text-align: center;
     cursor: pointer;
 
-    & span{
-        font-weight: 600;
+    & span {
+      font-weight: 600;
+    }
+  }
+
+  @media (max-width: ${media.desktop1680}) {
+    padding-top: 3%;
+    width: 48%;
+  }
+  @media (max-width: ${media.desktop1366}) {
+    h2 {
+      font-size: 1.6em;
     }
 
-}
+    a {
+      font-size: 0.95em;
+    }
+    padding-top: 6%;
+    width: 45%;
+  }
 
+  @media (max-width: ${media.tablet1024}) {
+    padding-top: 9%;
+    width: 60%;
+    a {
+      font-size: 0.86em;
+    }
+  }
 
+  @media (max-width: ${media.mobile651}) {
+    padding-top: 14%;
+    width: 85%;
+    a {
+      font-size: 0.75em;
+    }
+  }
+
+  @media (max-width: ${media.mobile375}) {
+    padding-top: 22%;
+    padding-bottom: 10%;
+    width: 95%;
+  }
 `;
 
 const FormRow = styled.div`
 
-display: flex;
-flex-wrap: no-wrap;
-justify-content: space-between;
-flex-direction: column;
+  display: flex;
+  flex-wrap: no-wrap;
+  justify-content: space-between;
+  flex-direction: column;
 
-@media ${media.mediumDevice} {
+  @media ${media.mediumDevice} {
     flex-direction: row;
-}
+  }
 `;
 
 const CrearCuenta = styled.form`
-padding-top: 4%;
+  padding-top: 4%;
 `;
 
-
-export {
-    DataContainer,
-    FormRow,
-    CrearCuenta,
-};
+export { DataContainer, FormRow, CrearCuenta };
